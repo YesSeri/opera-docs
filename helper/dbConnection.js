bab91552401a51a15b88ee95aa4221d0e747a3f2
YesSeri <henrik.zenkert@gmail.com>
2020-09-12 22:34:31 +0200

    changed to remote instead of local DB

--- helper/dbConnection.js
+++ helper/dbConnection.js
@@ -1,11 +1,7 @@
 const mysql = require('mysql2');
 let connection = ''
-if (process.env.NODE_ENV !== 'production') {
-    connection = mysql.createConnection(process.env.LOCAL_DB_CONNECTION_URL)
-} else {
-
-    connection = mysql.createConnection(process.env.DB_CONNECTION_URL)
-}
+    // connection = mysql.createConnection(process.env.LOCAL_DB_CONNECTION_URL)
+connection = mysql.createConnection(process.env.DB_CONNECTION_URL)
 
 
 connection.connect(function(err) {
