41f8026d8916649a0b86e6935e07303e287af411
YesSeri <henrik.zenkert@gmail.com>
2020-09-12 20:16:30 +0200

    Adjusted some small mistakes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors')
 
@@ -12,7 +13,11 @@ const operaRoutes = require('./routes/operaRoutes.js');
 
 app.use(cors())
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, 'build')));
+  const folder = path.join(__dirname, 'client', 'build')
+  app.use(express.static(path.join(folder)));
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(folder, 'index.html'))
+  })
 }
 
 app.use('/api/pieces', piecesRoutes);
