ca214988f239ed93b02e1d6a053e76ad982d36af
YesSeri <henrik.zenkert@gmail.com>
2020-09-12 23:54:16 +0200

    Fixed it kind of. Shows a frame, and is downloadable, and renders if you have a broswer that can do that.

--- helper/dbConnection.js
+++ helper/dbConnection.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql2');
-let connection = ''
     // connection = mysql.createConnection(process.env.LOCAL_DB_CONNECTION_URL)
-connection = mysql.createConnection(process.env.DB_CONNECTION_URL)
+// const connection = mysql.createConnection(process.env.DB_CONNECTION_URL)
+const connection  = mysql.createPool(process.env.DB_CONNECTION_URL);
+//   var pool  = mysql.createPool({
+//     connectionLimit : 10,
+//     host            : 'example.org',
+//     user            : 'bob',
+//     password        : 'secret',
+//     database        : 'my_db'
+//   });
 
-
-connection.connect(function(err) {
-    if (err) console.error(err);
-    console.log('Connected to DB')
-});
+// connection.connect(function(err) {
+//     if (err) console.error(err);
+//     console.log('Connected to DB')
+// });
 
 module.exports = connection;
