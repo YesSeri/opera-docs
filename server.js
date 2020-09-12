fdc8e90d8d15166e39aa4be21c4802b7c243820d
YesSeri <henrik.zenkert@gmail.com>
2020-09-12 20:21:13 +0200

    Moved how react is served by server to after the server API get calls.

--- server.js
+++ server.js
@@ -11,7 +11,14 @@ const piecesRoutes = require('./routes/piecesRoutes.js');
 const composersRoutes = require('./routes/composersRoutes.js');
 const operaRoutes = require('./routes/operaRoutes.js');
 
-app.use(cors())
+if(process.env.NODE_ENV !== 'production'){
+  app.use(cors())
+}
+
+app.use('/api/pieces', piecesRoutes);
+app.use('/api/composers', composersRoutes);
+app.use('/api/operas', operaRoutes);
+
 if(process.env.NODE_ENV === 'production'){
   const folder = path.join(__dirname, 'client', 'build')
   app.use(express.static(path.join(folder)));
@@ -19,11 +26,6 @@ if(process.env.NODE_ENV === 'production'){
     res.sendFile(path.resolve(folder, 'index.html'))
   })
 }
-
-app.use('/api/pieces', piecesRoutes);
-app.use('/api/composers', composersRoutes);
-app.use('/api/operas', operaRoutes);
-
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`READServer listening at port ${port}`));
