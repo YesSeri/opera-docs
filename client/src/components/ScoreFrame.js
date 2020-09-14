b6daaa0b1f40e52f383cee16d4726d7095894e9d
YesSeri <henrik.zenkert@gmail.com>
2020-09-14 13:39:12 +0200

    Reload PDF every three seconds if onload event not triggered. This is because google pdf viewer doesnt always load as it should. Long term  I should look into finding a better way of displaying pdfs.

--- client/src/components/ScoreFrame.js
+++ client/src/components/ScoreFrame.js
@@ -1,60 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/scoreFrame.css';
-import { PDFObject } from 'react-pdfobject';
 
-class ScoreFrame extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      loading: true,
-      fileURL: null,
-      fileExists: true,
-      random: 0,
-    };
-  }
-  componentDidMount(){
-    const timer = setTimeout(() => {
-      console.log('This will run after 1 second!')
-    }, 1000);
-    return () => clearTimeout(timer);
-  }
-  handleLoad = () => {
-    console.log('handleLoad');
-    this.setState({ loading: false });
+function ScoreFrame(props) {
+  const [loading, setLoading] = useState(true);
+  const googleUrlPDF = `https://drive.google.com/viewerng/viewer?embedded=true&url=https://singcademy.com/wp-content/uploads/pdfsToBeAccessed/${props.filename}`;
+  let timer = null;
+
+  useEffect(() => {
+    timer = setInterval(() => {
+      reloadIframe();
+    }, 3000);
+  }, []);
+
+  const reloadIframe = () => {
+    var iframe = document.getElementById('myIframe');
+    iframe.src = iframe.src;
+  };
+  const handleLoad = () => {
+    setLoading(false);
+    clearInterval(timer);
   };
 
-  resetIframe() {
-    this.setState({ random: this.state.random + 1 });
-  }
-
-  renderIframe = () => {
-    const googleUrlPDF = `https://drive.google.com/viewerng/viewer?embedded=true&url=https://singcademy.com/wp-content/uploads/pdfsToBeAccessed/${this.props.filename}`;
-    // const googleUrlPDF = `https://docs.google.com/gview?url=${this.props.filename}&embedded=true`
-    console.log(googleUrlPDF);
+  const renderIframe = () => {
     return (
       <div className="iframeContainer">
         <iframe
-          style={{visibility: this.state.loading ? "hidden" : "visible"}}
+          style={{ visibility: loading ? 'hidden' : 'visible' }}
           id="myIframe"
-          key={this.state.random}
           src={googleUrlPDF}
           title="embededPDF"
-          onLoad={this.handleLoad}
+          onLoad={handleLoad}
         />
       </div>
     );
   };
-
-  render() {
-    return (
-      <div className="container">
-        <div className="innerContainer">
-          {/* {this.state.loading ? <a href={googleUrlPDF}>Download File</a> : null} */}
-          {this.renderIframe()}
-        </div>
+  return (
+    <div className="container">
+      <div className="innerContainer">
+        {loading ? <a href={googleUrlPDF}>Download File</a> : null}
+        {renderIframe()}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default ScoreFrame;
