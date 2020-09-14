6d8d23f34d2104c4adf4ac3b2181e263171a2d6a
YesSeri <henrik.zenkert@gmail.com>
2020-09-14 13:11:47 +0200

    Before converting scoreframe to function

--- client/src/components/ScoreFrame.js
+++ client/src/components/ScoreFrame.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './css/scoreFrame.css';
-import { PDFObject } from 'react-pdfobject'
+import { PDFObject } from 'react-pdfobject';
 
 class ScoreFrame extends React.Component {
   constructor(props) {
@@ -9,34 +9,48 @@ class ScoreFrame extends React.Component {
       loading: true,
       fileURL: null,
       fileExists: true,
-      random: 0
+      random: 0,
     };
   }
   componentDidMount(){
+    const timer = setTimeout(() => {
+      console.log('This will run after 1 second!')
+    }, 1000);
+    return () => clearTimeout(timer);
   }
+  handleLoad = () => {
+    console.log('handleLoad');
+    this.setState({ loading: false });
+  };
 
   resetIframe() {
-    this.setState({random: this.state.random + 1});
-}
+    this.setState({ random: this.state.random + 1 });
+  }
 
-  hideSpinner = () => {
-    this.setState({
-      loading: false,
-    });
+  renderIframe = () => {
+    const googleUrlPDF = `https://drive.google.com/viewerng/viewer?embedded=true&url=https://singcademy.com/wp-content/uploads/pdfsToBeAccessed/${this.props.filename}`;
+    // const googleUrlPDF = `https://docs.google.com/gview?url=${this.props.filename}&embedded=true`
+    console.log(googleUrlPDF);
+    return (
+      <div className="iframeContainer">
+        <iframe
+          style={{visibility: this.state.loading ? "hidden" : "visible"}}
+          id="myIframe"
+          key={this.state.random}
+          src={googleUrlPDF}
+          title="embededPDF"
+          onLoad={this.handleLoad}
+        />
+      </div>
+    );
   };
 
   render() {
-    // const googleUrlPDF = `https://drive.google.com/viewerng/viewer?embedded=true&url=https://singcademy.com/wp-content/uploads/pdfsToBeAccessed/${this.props.filename}`;
-    const googleUrlPDF = `https://docs.google.com/gview?url=${this.props.filename}&embedded=true`
-    console.log(googleUrlPDF)
     return (
       <div className="container">
         <div className="innerContainer">
-          <div className="iframeContainer">
-            <PDFObject url={this.props.filename} />
-            {/* {<iframe key={this.state.random} src={googleUrlPDF} title="embededPDF" />} */}
-            {/* <button onClick={() => { this.resetIframe(); }}>Reset</button> */}
-          </div>
+          {/* {this.state.loading ? <a href={googleUrlPDF}>Download File</a> : null} */}
+          {this.renderIframe()}
         </div>
       </div>
     );
