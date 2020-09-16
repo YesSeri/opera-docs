a353ea3b1923615256744aeef4fd74434a3f3784
YesSeri <henrik.zenkert@gmail.com>
2020-09-17 00:45:45 +0200

    Trying browser pdfviewer

--- client/src/components/PdfFrame.js
+++ client/src/components/PdfFrame.js
@@ -4,14 +4,19 @@ import '@phuocng/react-pdf-viewer/cjs/react-pdf-viewer.css';
 
 export default function PdfFrame({ filename }) {
   const downloadLink = `https://singcademy.com/wp-content/uploads/pdfsToBeAccessed/${filename}`; // `https://singcademy.com/wp-content/uploads/pdfsToBeAccessed/${filename}`;
+  const frameStyles ={height:'100vh', width: '100vw'} 
   return (
-    // <embed style={{height:'98vh', width: '95vw'}} src={downloadLink}>
+    <>
 
-    // </embed>
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.4.456/build/pdf.worker.min.js">
-      <div style={{ height: '100vh' }}>
-        <Viewer fileUrl={downloadLink} />
-      </div>
-    </Worker>
+    <object style={frameStyles} data={downloadLink}>
+    <embed style={frameStyles} src={downloadLink}>
+    </embed>
+</object>
+    </>
+    // <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.4.456/build/pdf.worker.min.js">
+    //   <div style={{ height: '100vh' }}>
+    //     <Viewer fileUrl={downloadLink} />
+    //   </div>
+    // </Worker>
   );
 }
