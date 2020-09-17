49319ecfc4459a73462671789c3c0c604067ff19
YesSeri <henrik.zenkert@gmail.com>
2020-09-17 10:23:01 +0200

    styled pdf toolbar more

--- client/src/components/PdfFrame.js
+++ client/src/components/PdfFrame.js
@@ -21,15 +21,15 @@ const renderToolbar = (toolbarSlot) => {
         }}
       >
         {/* <div style={{ padding: '0 2px' }}>{toolbarSlot.previousPageButton}</div> */}
+        {/* <div style={{ padding: '0 2px' }}>{toolbarSlot.nextPageButton}</div> */}
+        <div style={{ padding: '0 2px' }}>{toolbarSlot.zoomOutButton}</div>
+        <div style={{ padding: '0 2px' }}>{toolbarSlot.zoomPopover}</div>
+        <div style={{ padding: '0 2px' }}>{toolbarSlot.zoomInButton}</div>
+        <div style={{ padding: '0 2px' }}>{toolbarSlot.fullScreenButton}</div>
+        <div style={{ padding: '0 2px' }}>{toolbarSlot.downloadButton}</div>
         <div style={{ padding: '0 2px' }}>
           {toolbarSlot.currentPage + 1} / {toolbarSlot.numPages}
         </div>
-        {/* <div style={{ padding: '0 2px' }}>{toolbarSlot.nextPageButton}</div> */}
-        {/* <div style={{ padding: '0 2px' }}>{toolbarSlot.zoomOutButton}</div> */}
-        <div style={{ padding: '0 2px' }}>{toolbarSlot.zoomPopover}</div>
-        {/* <div style={{ padding: '0 2px' }}>{toolbarSlot.zoomInButton}</div> */}
-        <div style={{ padding: '0 2px' }}>{toolbarSlot.fullScreenButton}</div>
-        <div style={{ padding: '0 2px' }}>{toolbarSlot.downloadButton}</div>
         <div style={{ padding: '0 2px' }}>{toolbarSlot.moreActionsPopover}</div>
       </div>
     </div>
