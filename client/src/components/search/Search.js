2b1da401b62a46f73891e5dfe185ee58d8f9d91c
YesSeri <henrik.zenkert@gmail.com>
2020-09-20 13:46:58 +0200

    Full width banner

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -7,12 +7,12 @@ import Container from 'react-bootstrap/Container';
 function Search() {
   const [searchValue, setSearchValue] = useState('');
   return (
-    <Container fluid className="searchContainer">
+    <>
       <h1>Search</h1>
       <Banner></Banner>
       <SearchBar setSearchValue={setSearchValue}></SearchBar>
       <SearchResults searchValue={searchValue}></SearchResults>
-    </Container>
+    </>
   );
 }
 
