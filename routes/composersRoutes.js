203ac96e02b601956bbb461f7164a9488a140f81
YesSeri <henrik.zenkert@gmail.com>
2021-01-16 13:58:04 +0100

    another lower case

--- routes/composersRoutes.js
+++ routes/composersRoutes.js
@@ -8,7 +8,7 @@ router.use(express.json());
 router.get('/', (req, res) => {
 	const query = `SELECT DISTINCT c.last_name, c.first_name, c.id
                   FROM Pieces as p INNER JOIN operas as o ON p.opera_id = o.id 
-                  INNER JOIN Composers as c ON o.composer_id = c.id
+                  INNER JOIN composers as c ON o.composer_id = c.id
                   ORDER BY c.last_name
                  `;
 	pool.query(query, (err, result) => {
@@ -21,7 +21,7 @@ router.get('/:last_name', (req, res) => {
 	const last_name = req.params.last_name.replace('_', ' ');
 	const query = `SELECT o.name as opera, o.id as opera_id, c.first_name, c.last_name 
                   FROM operas as o 
-                  INNER JOIN Composers as c 
+                  INNER JOIN composers as c 
                   ON o.composer_id = c.id WHERE c.last_name = ?`;
 	pool.query(query, last_name, (err, result) => {
 		res.json(result);
--- routes/operaRoutes.js
+++ routes/operaRoutes.js
@@ -9,7 +9,7 @@ router.use(express.json());
 router.get('/', (req, res) => {
 	// Show all operas, sort by name
 	const query = `SELECT o.name as opera, o.id as opera_id, c.last_name FROM operas as o 
-     INNER JOIN Composers as c 
+     INNER JOIN composers as c 
      ON c.id = o.composer_id 
      ORDER BY opera`;
 	pool.query(query, function (err, result) {
@@ -21,7 +21,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
 	const query = `SELECT p.id, p.title, p.type, p.placement, o.name, opera_id, c.last_name
   FROM Pieces as p INNER JOIN operas as o ON p.opera_id = o.id 
-  INNER JOIN Composers as c ON o.composer_id = c.id
+  INNER JOIN composers as c ON o.composer_id = c.id
      WHERE o.id = ? ORDER BY p.placement ASC;`;
 
 	const id = req.params.id;
