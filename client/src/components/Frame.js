d806b41d60038d03f20a1e7cc5b72d1a5591d781
YesSeri <henrik.zenkert@gmail.com>
2021-01-16 15:09:12 +0100

    testing something new

--- client/src/components/Frame.js
+++ client/src/components/Frame.js
@@ -3,16 +3,18 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./frame.css";
-import { PDFObject } from 'react-pdfobject'
+import { PDFObject } from "react-pdfobject";
 
 export default function Frame({ downloadLink }) {
-
 	return (
 		<div className="iframeContainer">
 			<Container fluid>
 				<Row>
 					<Col xl={{ span: 10, offset: 1 }}>
-						<PDFObject url={downloadLink} />
+						<PDFObject
+							url={downloadLink}
+							fallbackLink={`https://docs.google.com/gview?url=${downloadLink}&embedded=true`}
+						/>
 					</Col>
 				</Row>
 			</Container>
