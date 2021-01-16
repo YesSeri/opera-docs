10be99dec555b8277a16986802a401ad3cc75b33
YesSeri <henrik.zenkert@gmail.com>
2021-01-16 13:54:59 +0100

    trying to fix DB

--- routes/piecesRoutes.js
+++ routes/piecesRoutes.js
@@ -7,8 +7,8 @@ const pool = require('../utils/database');
 router.use(express.json());
 router.get('/arias', (req, res) => {
 	const query = `SELECT p.id as piece_id, p.title, p.type, p.placement, o.name as opera, o.id as opera_id, c.first_name, c.last_name, c.id as composer_id 
-                    FROM pieces as p INNER JOIN operas as o ON p.opera_id = o.id 
-                    INNER JOIN composers as c ON o.composer_id = c.id
+                    FROM Pieces as p INNER JOIN Operas as o ON p.opera_id = o.id 
+                    INNER JOIN Composers as c ON o.composer_id = c.id
                     WHERE p.type = 'aria'
                     ORDER BY opera ASC, p.placement ASC;`;
 	pool.query(query, function (err, result) {
@@ -20,8 +20,8 @@ router.get('/arias', (req, res) => {
 router.get('/:id', (req, res) => {
 	const id = req.params.id;
 	const query = `SELECT p.title, p.description, p.file_title, p.type, p.placement, o.name as opera, c.first_name, c.last_name 
-                 FROM pieces as p INNER JOIN operas as o ON p.opera_id = o.id 
-                 INNER JOIN composers as c ON o.composer_id = c.id WHERE p.id = ?;`;
+                 FROM Pieces as p INNER JOIN Operas as o ON p.opera_id = o.id 
+                 INNER JOIN Composers as c ON o.composer_id = c.id WHERE p.id = ?;`;
 	pool.query(query, id, (err, result) => {
 		if (err) console.error(err);
 		if (Object.keys(result).length === 0) {
