5c4d767d7fa5acd5f762638f5552991334ccdba5
YesSeri <henrik.zenkert@gmail.com>
2021-06-12 23:16:04 +0200

    minor css improvements.

--- client/src/stylesAppGlobal/styleApp.js
+++ client/src/stylesAppGlobal/styleApp.js
@@ -1,12 +1,10 @@
 import styled from 'styled-components/macro';
 
 export const Wrapper = styled.div`
-
 	display: flex;
 	min-height: 100vh;
 	flex-direction: column;
 	justify-content: space-between;
-
 	.siteContainer {
 		text-align: center;
 	}
