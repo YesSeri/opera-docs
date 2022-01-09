a3c19ff2b82ea89ebb82d32598d8edce2e7698a3
YesSeri <34012616+YesSeri@users.noreply.github.com>
2022-01-09 12:08:06 +0100

    changed database connection settings

--- utils/database.js
+++ utils/database.js
@@ -1,4 +1,14 @@
 const mysql = require('mysql2/promise');
-const pool = mysql.createPool(process.env.DB_CONNECTION_URL);
+// const pool = mysql.createPool(process.env.DB_CONNECTION_URL);
+const options = {
+	host: process.env.DB_URL,
+	user: process.env.DB_USER,
+	database: process.env.DB_NAME,
+	password: process.env.DB_PASS,
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0
+}
+const pool = mysql.createPool(options);
 
 module.exports = pool;
